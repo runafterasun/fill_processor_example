f1151de6d78ea4fc27374e1d0030f03817f22e4e
runafterasun <runafterasun@gmail.com>
2023-09-01 14:50:55 +0300

    add new test for primitive type arrays

--- src/main/java/org/example/objects/SimpleBoxTypePrimitive.java
+++ src/main/java/org/example/objects/SimpleBoxTypePrimitive.java
@@ -12,6 +12,56 @@ public class SimpleBoxTypePrimitive {
 
     private char primChar;
 
+    private int[] primIntArray;
+
+    private long[] primLongArray;
+
+    private boolean[] primBooleanArray;
+
+    private double[] primDoubleArray;
+
+    private char[] primCharArray;
+
+    public int[] getPrimIntArray() {
+        return primIntArray;
+    }
+
+    public void setPrimIntArray(int[] primIntArray) {
+        this.primIntArray = primIntArray;
+    }
+
+    public long[] getPrimLongArray() {
+        return primLongArray;
+    }
+
+    public void setPrimLongArray(long[] primLongArray) {
+        this.primLongArray = primLongArray;
+    }
+
+    public boolean[] getPrimBooleanArray() {
+        return primBooleanArray;
+    }
+
+    public void setPrimBooleanArray(boolean[] primBooleanArray) {
+        this.primBooleanArray = primBooleanArray;
+    }
+
+    public double[] getPrimDoubleArray() {
+        return primDoubleArray;
+    }
+
+    public void setPrimDoubleArray(double[] primDoubleArray) {
+        this.primDoubleArray = primDoubleArray;
+    }
+
+    public char[] getPrimCharArray() {
+        return primCharArray;
+    }
+
+    public void setPrimCharArray(char[] primCharArray) {
+        this.primCharArray = primCharArray;
+    }
+
     public char getPrimChar() {
         return primChar;
     }
--- src/test/java/SimpleBoxTypePrimitiveTest.java
+++ src/test/java/SimpleBoxTypePrimitiveTest.java
@@ -3,15 +3,44 @@ import org.junit.jupiter.api.Test;
 import ru.objectsfill.core.RandomValue;
 import ru.objectsfill.object_param.Fill;
 
-public class SimpleBoxTypePrimitiveTest {
+class SimpleBoxTypePrimitiveTest {
 
     @Test
-    public void fillObjectWithPrimitive() {
+    void fillObjectWithPrimitive() {
         SimpleBoxTypePrimitive simpleBoxTypeTestObj = RandomValue.fill(Fill.object(new SimpleBoxTypePrimitive()).gen());
 
         assert simpleBoxTypeTestObj.getPrimDouble() != 0.0d;
         assert simpleBoxTypeTestObj.getPrimInt() != 0;
         assert simpleBoxTypeTestObj.getPrimLong() != 0L;
         assert simpleBoxTypeTestObj.getPrimChar() != 0;
+    }
+
+    @Test
+    void fillPrimitiveArrayInClass() {
+        SimpleBoxTypePrimitive fill = RandomValue.fill(Fill.object(new SimpleBoxTypePrimitive()).gen());
+
+        assert fill.getPrimLongArray().length == 5;
+        assert fill.getPrimIntArray().length == 5;
+        assert fill.getPrimBooleanArray().length == 5;
+        assert fill.getPrimCharArray().length == 5;
+        assert fill.getPrimDoubleArray().length == 5;
+    }
+
+    @Test
+     void fillPrimitiveArray() {
+        Object[] fillInt = RandomValue.fillArray(Fill.object(int[].class).gen());
+        assert fillInt.length == 5;
+
+        Object[] fillLong = RandomValue.fillArray(Fill.object(long[].class).gen());
+        assert fillLong.length == 5;
+
+        Object[] fillChar = RandomValue.fillArray(Fill.object(char[].class).gen());
+        assert fillChar.length == 5;
+
+        Object[] fillBool = RandomValue.fillArray(Fill.object(boolean[].class).gen());
+        assert fillBool.length == 5;
+
+        Object[] fillDouble = RandomValue.fillArray(Fill.object(double[].class).gen());
+        assert fillDouble.length == 5;
     }
 }
